36ddec2d957e89dcf9369c95a53af3a96583f4cb
Александр <kozlov.alexander.63@gmail.com>
2024-09-26 08:04:22 +0300

    Add files via upload

--- module_2_while.py
+++ module_2_while.py
@@ -4,8 +4,10 @@ current_value = 0
 
 while current_value < len(my_list):
     if my_list[current_value] >= 0:
+        if my_list[current_value] == 0:
+            current_value += 1
+            continue
         print(my_list[current_value])
         current_value += 1
-        continue
     else:
         break
