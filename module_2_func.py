db97c4a0dd3478bce1ee89798d0cb153842ba74f
Александр <kozlov.alexander.63@gmail.com>
2024-09-27 09:24:44 +0300

    Add files via upload

--- module_2_func.py
+++ module_2_func.py
@@ -2,8 +2,9 @@
 def get_matrix(n, m, value):
         matrix = []
         for i in range(n):
+            matrix.append([])
             for j in range(m):
-                matrix.append(value)
+                matrix[i].append(value)
         return matrix
 
 
